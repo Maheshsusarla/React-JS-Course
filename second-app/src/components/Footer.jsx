18e0a437821a74725a27d8a1a7f599c70b20b1ed
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-09-01 10:09:15 +0530

    react js second  app with add compoents and some basic styles

---
+++ second-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Mahesh @2026 all right </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ second-app/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactJS from '../images/ReactJS.jpg'
+
+
+const Home = () => {
+  return (
+    <div>
+        <h1 >Home components</h1>
+        <img src={ReactJS} alt="" className='img' />
+    </div>
+  )
+}
+
+export default Home
